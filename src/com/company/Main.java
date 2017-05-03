de46b8cb66cf86e86e8129203ce8acbc16c521a3
Andrew Smiley <andrew.smiley@ge.com>
2017-05-03 16:12:36 -0400

    Stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    NetworkADT networkADT = new NetworkADT("network.txt");
+        networkADT.generateMST("Tampa");
+        System.out.println("Test");
+
     }
 }
--- src/com/company/NetworkADT.java
+++ src/com/company/NetworkADT.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -133,26 +134,260 @@ public class NetworkADT {
      * @param city1 the starting verticie
      * @param city2 the ending verticie
      */
-    public int shortestPath(String city1, String city2){
-        return -1;
-    }
+    public void shortestPath(String city1, String city2){
 
+
+//        return -1;
+    }
+//    public int getUnsettledCount(boolean unsettled[])
+//    {
+//        int count = 0;
+//        for (int index = 0; index < unsettled.length; index++)
+//        {
+//            if (unsettled[index])
+//            {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+//    public void primsAlgorithm(int adjacencyMatrix[][])
+//    {
+//        int evaluationVertex;
+//        for (int source = 1; source <= this.size; source++)
+//        {
+//            for (int destination = 1; destination <= this.size; destination++)
+//            {
+//                this.network[source][destination] = adjacencyMatrix[source][destination];
+//            }
+//        }
+//
+//        for (int index = 1; index <= numberofvertices; index++)
+//        {
+//            key[index] = INFINITE;
+//        }
+//        key[1] = 0;
+//        unsettled[1] = true;
+//        parent[1] = 1;
+//
+//        while (getUnsettledCount(unsettled) != 0)
+//        {
+//            evaluationVertex = getMimumKeyVertexFromUnsettled(unsettled);
+//            unsettled[evaluationVertex] = false;
+//            settled[evaluationVertex] = true;
+//            evaluateNeighbours(evaluationVertex);
+//        }
+//    }
+//
+//    private int getMimumKeyVertexFromUnsettled(boolean[] unsettled2)
+//    {
+//        int min = Integer.MAX_VALUE;
+//        int node = 0;
+//        for (int vertex = 1; vertex <= numberofvertices; vertex++)
+//        {
+//            if (unsettled[vertex] == true && key[vertex] < min)
+//            {
+//                node = vertex;
+//                min = key[vertex];
+//            }
+//        }
+//        return node;
+//    }
+//
+//    public void evaluateNeighbours(int evaluationVertex)
+//    {
+//
+//        for (int destinationvertex = 1; destinationvertex <= numberofvertices; destinationvertex++)
+//        {
+//            if (settled[destinationvertex] == false)
+//            {
+//                if (adjacencyMatrix[evaluationVertex][destinationvertex] != INFINITE)
+//                {
+//                    if (adjacencyMatrix[evaluationVertex][destinationvertex] < key[destinationvertex])
+//                    {
+//                        key[destinationvertex] = adjacencyMatrix[evaluationVertex][destinationvertex];
+//                        parent[destinationvertex] = evaluationVertex;
+//                    }
+//                    unsettled[destinationvertex] = true;
+//                }
+//            }
+//        }
+//    }
+//
+//    public void printMST()
+//    {
+//        System.out.println("SOURCE  : DESTINATION = WEIGHT");
+//        for (int vertex = 2; vertex <= numberofvertices; vertex++)
+//        {
+//            System.out.println(parent[vertex] + "\t:\t" + vertex +"\t=\t"+ adjacencyMatrix[parent[vertex]][vertex]);
+//        }
+//    }
+
+//     A utility function to find the vertex with minimum key
+//     value, from the set of vertices not yet included in MST
+    int minKey(int key[], Boolean mstSet[])
+    {
+
+        // Initialize min value
+        int min = Integer.MAX_VALUE, min_index=-1;
+
+        for (int v = 0; v < size; v++)
+            if (mstSet[v] == false && key[v] < min)
+            {
+                min = key[v];
+                min_index = v;
+            }
+
+        return min_index;
+    }
+    /**
+     * using Prim’s algorithm, generate the MST starting at
+     city and output for each node in the Graph aside from city, which city it connects
+     to, as well as the total cost of the MST
+     * @param city
+     */
     public void generateMST(String city){
+        // Array to store constructed MST
+//        int size= this.size;//getIndex(city);
+        int parents[] = new int[size];
+
+        // Key values used to pick minimum weight edge in cut
+        int key[] = new int [size];
+
+        // To represent set of vertices not yet included in MST
+        Boolean mstSet[] = new Boolean[size];
+
+        // Initialize all keys as INFINITE
+        for (int i = 0; i < size; i++)
+        {
+            key[i] = Integer.MAX_VALUE;
+            mstSet[i] = false;
+        }
+
+        // Always include first 1st vertex in MST.
+        key[0] = 0;//getIndex(city);
+        // picked as first vertex
+        parents[0] = getIndex(city);//-1; // First node is always root of MST
+
+        // The MST will have V vertices
+        for (int count = 0; count < size-1; count++)
+        {
+            // Pick thd minimum key vertex from the set of vertices
+            // not yet included in MST
+            int u = minKey(key, mstSet);
+
+            // Add the picked vertex to the MST Set
+            mstSet[u] = true;
+
+            // Update key value and parent index of the adjacent
+            // vertices of the picked vertex. Consider only those
+            // vertices which are not yet included in MST
+            for (int v = 0; v < size; v++)
+
+                // graph[u][v] is non zero only for adjacent vertices of m
+                // mstSet[v] is false for vertices not yet included in MST
+                // Update the key only if graph[u][v] is smaller than key[v]
+                if (network[u][v] > 0 && mstSet[v] == false &&
+                        network[u][v] <  key[v])
+                {
+                    parents[v]  = u;
+                    key[v] = network[u][v];
+                }
+        }
+//        printMST(parents);
+        for (int i = 1; i < size; i++) {
+            System.out.println("City "+cities[i]+" connects to  "+cities[parents[i]] );
+//            System.out.println(parents[i] + " - " + i + "    " +
+//                    network[i][parents[i]]);
+        }
+
 
     }
 
+//    /**
+//     * This is just a utility function to
+//     * @param parents
+//     */
+//    void printMST(int parents[])
+//    {
+//    }
+
+    /**
+     * This is our depth first search,
+     * @param city
+     */
     public void dfs(String city){
+        //create an array tracking our visited values
+        //set them all to false
+        //using the Boolean wrapper class
+        Boolean[] visted = new Boolean[size];
+        Arrays.fill(visted, false);
+        //this is our stack to store our visited city names
+        ArrayList<String> stack = new ArrayList<>();
+        int i= getIndex(city);
+        int j;
+        visted[i] =true;
+        System.out.println(city);
+        //basically we'll iterate while we have unvisted nodes
+        do{
+            //iterate the edges in reverse
+            for( j =size-1; j> -1; j--){
+                //do this in reverse, that way we do the depth first
+                if(network[i][j] > 0 && visted[j] == false){
+                    stack.add(cities[j]); //add to our visited stack
+                }
+            }
+            //update our index with the top of the stack
+            i = getIndex(stack.remove(stack.size()-1));//do the stack pop
+            //by popping we'll slowly backtrack
+            //now we'll do the backtracking piece as need be
+            System.out.println(cities[i]);//output visited node
+            visted[i]= true; //mark it as visited
+        }while(Arrays.asList(visted).contains(false));
 
     }
     public void rdfs(String city){
 
     }
+
+    /**
+     * This is our bredth first search. Vist all adjacent nodes, backtracking and working across our network
+     * @param city the starting vertice
+     */
     public void bfs(String city){
         //this is our queue because fuuuuuuuck java and it doesn't have a native queue implementation
-        LinkedList<String> visited = new LinkedList<>();
-        boolean[] visted = new boolean[size];
-
-
+        //create an array tracking our visited values
+        //set them all to false
+        //using the Boolean wrapper class
+        Boolean[] visted = new Boolean[size];
+        Arrays.fill(visted, false);
+        //this is our stack to store our visited city names
+        ArrayList<String> queue = new ArrayList<>();
+        int i= getIndex(city);
+        int j;
+        visted[i] =true;
+        System.out.println(city);
+        //basically we'll iterate while we have unvisted nodes
+        do{
+            //iterate the edges 
+            for( j=0; j <size; j++ ){
+                //get all the adjacent nodes
+                if(network[i][j] > 0 && visted[j] == false){
+                    queue.add(cities[j]); //add to our visited queue (enqueue
+                }
+            }
+            //update our index with the first value in the queue
+            i = getIndex(queue.remove(0));//do the dequeue
+            //now we'll do the backtracking
+            //by dequeuing we'll slowly backtrack until we reach a node we haven't visted yet
+            while(visted[i]==true && queue.size() >0){
+                i = getIndex(queue.remove(0));//do the dequeue
+            }
+            if (queue.size() >0) {
+                System.out.println(cities[i]);//output visited node
+                visted[i]= true; //mark it as visited
+            }
+        }while(!queue.isEmpty());
 
     }
 
@@ -166,7 +401,7 @@ public class NetworkADT {
             //reference for our line
             String line;
             int lineCount = 0;
-            int verticies;
+            int verticies = 0;
             int sublineCount = 0; //this is our subline count, just so we can track if we need to increment i (i keeps track of the 2D array index)
             int i=0; //keep track of our 2d index
             int j = 0;//keep track of our cities index
@@ -180,8 +415,8 @@ public class NetworkADT {
                     this.network = new int[verticies][verticies];
                     this.cities = new String[verticies];
                     lineCount++;
-                }else if(lineCount > 11){
-                    if(sublineCount > 11){
+                }else if(lineCount > verticies){
+                    if(sublineCount > verticies-1){
                         i++; //move to the next index of the network
                         sublineCount = 0; //reset the subline count
                         network[i][sublineCount] = Integer.parseInt(line); //add to our network
